ac2f9efadf8af8b2a185f675e518dc0230a0eb73
Misty <gyc990326@gmail.com>
2018-03-12 10:45:24 +0800

    initialize folder structure & upload part of challenges

---
+++ challenges/crypto/baby_N1ES/challenge.py
@@ -0,0 +1,7 @@
+from N1ES import N1ES
+import base64
+key = "wxy191iss00000000000cute"
+n1es = N1ES(key)
+flag = "N1CTF{*****************************************}"
+cipher = n1es.encrypt(flag)
+print base64.b64encode(cipher)  # HRlgC2ReHW1/WRk2DikfNBo1dl1XZBJrRR9qECMNOjNHDktBJSxcI1hZIz07YjVx
